8c54243f755461f5036e77b355634890f0aee11a
mk53nnn-svg <mk53nnn@gmail.com>
2026-07-03 11:15:42 +0900

    ホームボタン追加スクリプト

---
+++ add_nav.php
@@ -0,0 +1,15 @@
+<?php
+$files = [
+  __DIR__ . '/public/order_input.php',
+  __DIR__ . '/public/summary.php',
+  __DIR__ . '/public/product_detail.php',
+  __DIR__ . '/public/master.php',
+  __DIR__ . '/public/initial_stock_input.php',
+];
+$nav = '<div style="padding:8px 16px;background:#fff;border-bottom:1px solid #eee;"><a href="index.php" style="font-size:12px;color:#888;text-decoration:none;">&laquo; ホーム</a></div>';
+foreach ($files as $f) {
+  $c = file_get_contents($f);
+  $c = str_replace('<body>', '<body>' . $nav, $c);
+  file_put_contents($f, $c);
+  echo $f . " done\n";
+}
